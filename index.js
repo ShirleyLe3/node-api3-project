17bbedfb88933cdce20f22826a328e64742df473
Shirley Lee <synchrotronflux@yahoo.com>
2020-10-10 19:19:44 -0400

    Progress

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code away!
+// - Write and implement four custom `middleware` functions, detailed below.
+// - Build an API to let clients perform CRUD operations on `users`.
+// - Add endpoints to retrieve the list of `posts` for a `user` and to store a new `post` for a `user`.
+const express = require("express");
+const userRouter = require("../users/userRouter");
+
+const server = express();
+const port = 4000;
+
+server.use(express.json());
+server.use(userRouter);
+
+server.use((req, res, next) => {
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${req.ip} ${req.method} ${req.url}`);
+
+  next();
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,144 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 
-router.post('/', (req, res) => {
-  // do your magic!
+//      create
+router.post("/", validateUser(), (req, res) => {
+  res.status(201).json(req.user);
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validateUserId(), validatePost(), (req, res) => {
+  res.status(200).json(post); //send back the post   and 201 for success create
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+//      read
+router.get("/", (req, res) => {
+  userDb
+    .get()
+    .then((users) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error retrieving",
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validateUserId(), (req, res) => {
+  res.status(200).json(req.user); // attach passed data
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", validateUserId(), (req, res) => {
+  userDb
+    .getUserPosts(req.user.id)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Error retrieving",
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+//      delete
+router.delete("/:id", validateUserId(), (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", validateUserId(), (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (!changes.name) {
+    res.status(400).json({
+      message: "Failed to provide username",
+    });
+  } else {
+    userDB
+      .update(id, changes)
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error: "Error updating the user",
+        });
+      });
+  }
 });
+// // CUSTOM MIDDLEWARE
+// `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+// this middleware runs on every request made to the API
 
-//custom middleware
-
-function validateUserId(req, res, next) {
-  // do your magic!
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}]
+    ${req.method} to ${req.url} from ${req.get("Origin")}`
+  );
+  next();
 }
 
-function validateUser(req, res, next) {
-  // do your magic!
+// all endpoints that include an `id` parameter in the url (ex: `/api/users/:id`)
+
+function validateUserId() {
+  //  if the `id` parameter is valid, store that user object as `req.user`
+  //  if the `id` parameter does not match any user id in the database, cancel the request and respond with status `400` and `{ message: "invalid user id" }`
+  return (req, res, next) => {
+    userDb.getById(req.params.id).then((user) => {
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      } else if (!user.id) {
+        return res.status(400).json({
+          message: "Invalid user ID",
+        });
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  };
+}
+
+function validateUser() {
+  //  if the request `body` is missing, cancel the request and respond with status `400` and `{ message: "missing user data" }`
+  //  if the request `body` is missing the required `name` field, cancel the request and respond with status `400` and `{ message: "missing required name field" }`
+  return (req, res, next) => {
+    if (!req.body) {
+      return res.status(400).json({
+        message: "Missing user data",
+      });
+      next();
+    } else if (!req.body.name) {
+      return res.status(400).json({
+        message: "Missing field",
+      });
+    } else
+      userDb
+        .insert(req.body)
+        .then((user) => {
+          req.user = user;
+          next();
+        })
+        .catch((error) => {
+          res.status(500).json({ message: "Error retrieving" });
+        });
+  };
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  // if the request `body` is missing, cancel the request and respond with status `400` and `{ message: "missing post data" }`
+  //  if the request `body` is missing the required `text` field, cancel the request and respond with status `400` and `{ message: "missing required text field" }
+  if (!res.body.text) {
+    return res.status(400).json({
+      message: "Missing post data",
+    });
+    next();
+  }
 }
 
 module.exports = router;
